cc753d8327bec88b720bdf7f904dc19c67a60b42
Paulo H Souza <paulosouza.ti@gmail.com>
2020-03-18 16:11:10 -0300

    spring initializr

---
+++ backend/src/test/java/br/com/devpaulosouza/easybuy/EasybuyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devpaulosouza.easybuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

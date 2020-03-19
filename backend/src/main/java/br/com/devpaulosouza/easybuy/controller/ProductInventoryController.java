732e2936d2f36dd7ccb99caaf0fd0b0a5317aaa0
Paulo H Souza <paulosouza.ti@gmail.com>
2020-03-18 23:22:59 -0300

    method create renamed to updateInventory, and @POST updated to @PUT

--- backend/src/main/java/br/com/devpaulosouza/easybuy/controller/ProductInventoryController.java
+++ backend/src/main/java/br/com/devpaulosouza/easybuy/controller/ProductInventoryController.java
@@ -5,10 +5,7 @@ import br.com.devpaulosouza.easybuy.dto.ProductInventoryOutputDto;
 import br.com.devpaulosouza.easybuy.service.ProductInventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
@@ -20,8 +17,8 @@ public class ProductInventoryController {
     @Autowired
     ProductInventoryService service;
 
-    @PostMapping
-    public Mono<ResponseEntity<ProductInventoryOutputDto>> create(@Valid @RequestBody ProductInventoryInputDto productDto) {
+    @PutMapping
+    public Mono<ResponseEntity<ProductInventoryOutputDto>> updateInventory(@Valid @RequestBody ProductInventoryInputDto productDto) {
         return service
                 .updateInventory(productDto)
                 .map(ResponseEntity::ok);

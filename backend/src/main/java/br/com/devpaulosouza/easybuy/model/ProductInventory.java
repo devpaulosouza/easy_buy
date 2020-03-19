443e96670d8748ff6f16cdd99dba432c5c93ee79
Paulo H Souza <paulosouza.ti@gmail.com>
2020-03-18 23:34:09 -0300

    removed unused column "updated_at" from table product_inventory

--- backend/src/main/java/br/com/devpaulosouza/easybuy/model/ProductInventory.java
+++ backend/src/main/java/br/com/devpaulosouza/easybuy/model/ProductInventory.java
@@ -3,30 +3,53 @@ package br.com.devpaulosouza.easybuy.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Entity;
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
 
 @Entity
 @Table
 @Getter
 @Setter
 @ToString
-public class ProductInventory extends AbstractEntity {
+public class ProductInventory implements br.com.devpaulosouza.easybuy.model.Entity {
 
-    @JoinColumn(name = "product_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private UUID uuid;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    @PrePersist
+    public void prePersist() {
+        if (Objects.isNull(this.getUuid())) {
+            this.setUuid(UUID.randomUUID());
+        }
+    }
+
+    @JoinColumn(name = "product_id", updatable = false)
     @OneToOne
     Product product;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     BigDecimal quantity;
 
-    @Column
+    @Column(updatable = false)
     BigDecimal previousQuantity;
 
     // algum dia (ou nunca) encontro um nome melhor pra isso
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     BigDecimal quantityOffset;
 
 }
